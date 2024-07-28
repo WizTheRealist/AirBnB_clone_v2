5be7f7fcf105d71adef3e76f5593c1f5602e7007
Chimaobi <wiztherealist@gmail.com>
2024-07-28 13:12:19 -0700

    flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,6 +6,8 @@ Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
     /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+    /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+    /number/<n>: display “n is a number” only if n is an integer
 """
 
 from flask import Flask
@@ -27,6 +29,18 @@ def c(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace('_', ' ')
     return "C {}".format(text)
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>."""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """display “n is a number” only if n is an integer"""
+    return "{} is a number".format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
